2c4a51933302a640a40ca88f0a629938122f4a62
urk21cs2039 <110082561+urk21cs2039@users.noreply.github.com>
2022-09-07 11:15:33 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,54 +1,3 @@
-class Node:
-    def __init__(self, data=None):
-        self.data = data
-        self.previous = self
-        self.next = self
-
-
-class DoublyCircularLinkedList:
-    def __init__(self):
-        self.head = None
-        self.count = 0
-
-    def add_at_tail(self, data) -> bool:
-        # Write code here
-
-    def add_at_head(self, data) -> bool:
-        # Write code here
-
-    def add_at_index(self, index, data) -> bool:
-        # Write code here
-
-    def get(self, index) -> int:
-        # Write code here
-
-    def delete_at_index(self, index) -> bool:
-        # Write code here
-
-    def get_previous_next(self, index) -> list:
-        # Write code here
-
-
-# Do not change the following code
-operations = []
-for specific_operation in input().split(','):
-    operations.append(specific_operation.strip())
-input_data = input()
-data = []
-iteration_count = 0
-
-for item in input_data.split(', '):
-    inner_list = []
-    if item.isnumeric():
-        data.append(int(item))
-    elif item.startswith('['):
-        item = item[1:-1]
-        for letter in item.split(','):
-            if letter.isnumeric():
-                inner_list.append(int(letter))
-        data.append(inner_list)
-
-obj = DoublyCircularLinkedList()
 result = []
 for i in range(len(operations)):
     if operations[i] == "add_at_head":
@@ -63,5 +12,4 @@ for i in range(len(operations)):
         result.append(obj.get_previous_next(data[i]))
     elif operations[i] == 'delete_at_index':
         result.append(obj.delete_at_index(data[i]))
-
 print(result)
